ccf3932e73587fdfb5755fcc5ec12e727f163890
unknown <bwen761@aucklanduni.ac.nz>
2025-07-03 20:45:16 +1200

    Added MiddleWare setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 import userRoutes from './routes/user.route.js';
 import adminRoutes from './routes/admin.route.js';
@@ -17,6 +18,8 @@ const app = express();
 const PORT = process.env.PORT 
 
 app.use(express.json());
+app.use(clerkMiddleware()) // this will add auth to request object => req.auth.userId
+
 
 app.use("/api/users", userRoutes)
 app.use("/api/auth", authRoutes)
