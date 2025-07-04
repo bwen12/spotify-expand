27cb8be669161a969247a640a7f7ffd40a85d576
unknown <bwen761@aucklanduni.ac.nz>
2025-07-04 17:13:43 +1200

    Album routes

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,11 +1,11 @@
 import {Router} from 'express';
+import { getAllAlbums, getAlbumById } from '../controller/album.controller.js';
 
 const router = Router();
 
 
-router.get('/', (req, res) => {
-    res.json({message: 'Album route is working!'}); 
-});
+router.get('/', getAllAlbums);
+router.get('/:albumId', getAlbumById);
 
 
 export default router;
