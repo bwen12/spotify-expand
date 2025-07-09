2f8915e1cb017ac5b68dc8cfeecd528e91dccb39
unknown <bwen761@aucklanduni.ac.nz>
2025-07-09 18:25:50 +1200

    added album page

--- frontend/spotify-clone/src/pages/AlbumPage.tsx
+++ frontend/spotify-clone/src/pages/AlbumPage.tsx
@@ -1,12 +1,14 @@
 import { useParams } from "react-router-dom";
 import Topbar from "../components/ui/ui/Topbar";
 import { useMusicStore } from "@/stores/useMusicStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
 
 const AlbumPage = () => {
-  const {albumId} = useParams();
-  const {fetchAlbumById, currentAlbum, isLoading} = useMusicStore();
-  
+  const { albumId } = useParams();
+  const { fetchAlbumById, currentAlbum, isLoading } = useMusicStore();
+
   useEffect(() => {
     if (albumId) {
       fetchAlbumById(albumId);
@@ -14,14 +16,31 @@ const AlbumPage = () => {
   }, [albumId, fetchAlbumById]);
 
   if (isLoading) return null;
-  
+
   return (
     <div>
       <Topbar />
       {/* Main content for the album page */}
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-3xl font-bold text-white">Album Page</h1>
-        <p className="text-gray-400 mt-2">This is the album page.</p>
+      <div className="h-screen">
+        <ScrollArea className="h-full">
+          <div className="relative min-h-full">
+            {/* gradient here */}
+            <div
+              className="absolute inset-0 bg-gradient-to-b from-[#5038a0]/80 rounded-lg via-zinc-900/80 to-zinc-900 pointer-events-none"
+              aria-hidden="true"
+            />
+            {/* Showing the actual album and text and etc */}
+            <div className="relative z-10">
+              <div className="flex p-6 g-6 pb-8">
+                <img
+                  src={currentAlbum?.imageUrl}
+                  alt="Album Cover"
+                  className="w-[240px] h-[240px] rounded-lg shadow-lg"
+                />
+              </div>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
